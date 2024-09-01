f2c68480ffa8c3e58d7246b798bb0648087ea3e9
Kareemayman <Kareem.Mohamed.ayman@gmail.com>
2024-09-01 22:56:49 +0300

    Commit Files

---
+++ script.js
@@ -0,0 +1,35 @@
+const add = document.querySelector("button:first-of-type")
+const clear = document.querySelector("button:last-of-type")
+const inputText = document.querySelector("input[type=text]")
+const ul = document.querySelector("ul")
+
+add.addEventListener("click", (e) => {
+  let newItem = document.createElement("li")
+  newItem.innerHTML = `<div>
+  <input type='checkbox'>
+  <label>${inputText.value}</label>
+  </div>
+  <button class='del'>Delete</button>`
+  ul.appendChild(newItem)
+
+  inputText.value = ""
+})
+
+clear.addEventListener("click", (e) => {
+  ul.innerHTML = ""
+})
+
+// Using event delegation on created delete buttons
+document.body.addEventListener("click", (e) => {
+  if (e.target.matches("button.del")) {
+    console.log("delete button clicked")
+    e.target.closest("li").remove()
+  } else if (e.target.matches("div label")) {
+    console.log("label clicked")
+    if (e.target.previousElementSibling.checked) {
+      e.target.previousElementSibling.checked = false
+    } else {
+      e.target.previousElementSibling.checked = true
+    }
+  }
+})
